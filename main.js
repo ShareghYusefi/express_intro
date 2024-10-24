cb45b7719df1d9ee4aacce46cf46484fa4f5f0f9
Sharegh Yusefi <48734749+ShareghYusefi@users.noreply.github.com>
2024-10-23 18:00:37 -0600

    custom middlewares

--- main.js
+++ main.js
@@ -4,9 +4,22 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+function custom_middleware(req, res, next) {
+  console.log("Middleware function called");
+  // We need to call the next function so the request doesnt hang
+  next();
+}
+
+// Use the middleware function for all routes
+app.use(custom_middleware);
+
+app.get(
+  "/",
+  // this is a middleware function for the / route
+  function (req, res) {
+    res.send("Hello World");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
